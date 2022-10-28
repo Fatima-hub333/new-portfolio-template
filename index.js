de24e6b45390b93f95dde702bb898bd442cdefba
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-28 09:56:00 +0500

    update index.js

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const allSection = document.querySelector('.main-content');
 function PageTransitions() {
   //Button click active class
   for (let i = 0; i < sectBtn.length; i++){
-    sectBtn[i].addEventListener('click', () => {
+    sectBtn[i].addEventListener('click', function() {
       let currentBtn = document.querySelectorAll('.active-btn');
-      currentBtn[0].classList = currentBtn[0].className.replace('active')
+      currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
+      this.className += 'active-btn';
     })
   }
 }
+
+PageTransitions();
